1fb520c9822797e4c2975941fb036a620dbd8736
Sanjay H R <sanjudhyan@gmail.com>
2019-08-22 23:16:47 +0530

    Added Jest Unit Testing for App.js

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -5,10 +5,17 @@
 import 'react-native';
 import React from 'react';
 import App from '../App';
+import {NativeEventEmitter} from 'react-native';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
+import { JestEnvironment } from '@jest/environment';
 
 it('renders correctly', () => {
   renderer.create(<App />);
 });
+
+jest.mock('NativeEventEmitter');
+
+const nativeEmitter = new NativeEventEmitter();
+nativeEmitter.emit('sayHello');
